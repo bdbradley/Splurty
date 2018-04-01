c640021a700065e623ce7f44755aadacb6f3b298
Barry Bradley <bbradley13@gmail.com>
2018-04-01 22:32:56 +0000

    Updated gems subject to previous Loofah version vulnerability

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,11 +1,13 @@
 class QuotesController < ApplicationController
   def index
+    # Takes sheet and puts in random order, then grabs first one
     @quote = Quote.order("RANDOM()").first
   end
 
   
 
   def create
+    #Creates quote in db with user parameters
      @quote = Quote.create(quote_params)
       if @quote.invalid?
         flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
@@ -19,6 +21,7 @@ class QuotesController < ApplicationController
   private
 
   def quote_params
+    #Permits user to add saying and author
     params.require(:quote).permit(:saying, :author)
   end
 end
